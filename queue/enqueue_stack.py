98916678b8d5e9856b09bc3daf6e06ea9466a99c
Takashi Maeda <eclipsehal2000@gmail.com>
2024-02-04 08:43:54 +0900

    enqueue_stack

---
+++ queue/enqueue_stack.py
@@ -0,0 +1,51 @@
+
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    # WRITE ENQUEUE MEHTOD HERE #
+    #                           #
+    #                           #
+    #                           #
+    #                           #
+    #############################
+    def enqueue(self, value):
+        while self.stack1:
+            temp_value = self.stack1.pop()
+            self.stack2.append(temp_value)
+
+        self.stack1.append(value)
+        while self.stack2:
+            temp_value = self.stack2.pop()
+            self.stack1.append(temp_value)
+
+    def peek(self):
+        return self.stack1[-1]
+
+    def is_empty(self):
+        return len(self.stack1) == 0
+
+
+# Create a new queue
+q = MyQueue()
+
+# Enqueue some values
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Output the front of the queue
+print("Front of the queue:", q.peek())
+
+# Check if the queue is empty
+print("Is the queue empty?", q.is_empty())
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Front of the queue: 1
+    Is the queue empty? False
+    
+"""
