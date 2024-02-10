e6b03ed66894ecbb6a8d5e0c13131d46133705d4
Takashi Maeda <eclipsehal2000@gmail.com>
2024-02-11 07:49:29 +0900

    hash table constructor and set item

---
+++ hashtable/constructer.py
@@ -0,0 +1,19 @@
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None]*size
+        print(self.data_map)
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter)*23 % len(self.data_map))
+        return my_hash
+
+    def print_table(self):
+
+        for i, val in enumerate(self.data_map):
+            print(i, ":", val)
+
+
+hashes = HashTable()
+hashes.print_table()
