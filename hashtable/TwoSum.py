42a2039c1b47ac36a6c93bf6e03ccd1f45575817
Takashi Maeda <eclipsehal2000@gmail.com>
2024-02-18 10:14:11 +0900

    commitizen test

---
+++ hashtable/TwoSum.py
@@ -0,0 +1,44 @@
+# Problem:
+# Given an array of integers nums and a target integer target, find the indices of two numbers in the array that add up to the target.
+# The main challenge here is to implement this function in one pass through the array. This means you should not iterate over the array more than once. Therefore, your solution should have a time complexity of O(n), where n is the number of elements in nums.
+
+
+# WRITE TWO_SUM FUNCTION HERE #
+#                             #
+#                             #
+#                             #
+#                             #
+###############################
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
+
+
+print(two_sum([5, 1, 7, 2, 9, 3], 10))
+print(two_sum([4, 2, 11, 7, 6, 3], 9))
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))
+print(two_sum([1, 3, 5, 7, 9], 10))
+print(two_sum([1, 2, 3, 4, 5], 10))
+print(two_sum([1, 2, 3, 4, 5], 7))
+print(two_sum([1, 2, 3, 4, 5], 3))
+print(two_sum([], 0))
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 4]
+    [1, 3]
+    [0, 3]
+    [1, 3]
+    []
+    [2, 3]
+    [0, 1]
+    []
+
+"""
