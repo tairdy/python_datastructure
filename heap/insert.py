b8906f829b00f046a31e0c3fd132952508b42f98
Takashi Maeda <eclipsehal2000@gmail.com>
2024-03-17 04:57:27 +0900

    heap basics

---
+++ heap/insert.py
@@ -0,0 +1,35 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap)-1
+
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+
+myheap = MaxHeap()
+myheap.insert(10)
+myheap.insert(30)
+myheap.insert(20)
+myheap.insert(15)
+myheap.insert(5)
+myheap.insert(200)
+
+
+print(myheap.heap)
