de06890452fa7b4a6f5b389ecac5e5c98656b15a
Takashi Maeda <eclipsehal2000@gmail.com>
2024-05-02 06:54:49 +0900

    basic sort

---
+++ sort/selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(my_list):
+    for i in range(len(my_list)-1):
+        min_index = i
+        for j in range(i+1, len(my_list), 1):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+
+        # swap min Index
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+    return my_list
