a866eabc7f34831567c8b5053e5435b944b03aef
Takashi Maeda <eclipsehal2000@gmail.com>
2024-05-02 05:15:20 +0900

    tree traverse all

---
+++ TreeTraversal/DepthFirstSearch_InOrder.py
@@ -0,0 +1,13 @@
+
+def dfs_in_order(self):
+    results = []
+
+    def traverse(current_node):
+        if current_node.left is not None:
+            traverse(current_node.left)
+        results.append(current_node.value)
+        if current_node.right is not None:
+            traverse(current_node.right)
+
+    traverse(self.root)
+    return results
